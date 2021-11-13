1502feeccbd54db5bb105ef789f98211def31beb
shubham khanna <shubam20khanna@gmail.com>
2021-11-14 02:10:44 +0530

    Added code for user dashboard.

--- src/main/java/com/example/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/example/smartcontactmanager/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.example.smartcontactmanager.controller;
 
+import com.example.smartcontactmanager.dao.UserRepository;
+import com.example.smartcontactmanager.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("/index")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principle) {
+        String userName = principle.getName();
+        // get the user details using username(email).
+        User user = userRepository.getUserByName(userName);
+        model.addAttribute("user", user);
         return "user_dashbaord";
     }
 }
